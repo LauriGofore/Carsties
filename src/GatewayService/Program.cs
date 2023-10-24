ae4020692c8a6a3eda11f455b49aef2cea937fc0
Lauri Saarela <lauri.saarela@gofore.com>
2023-10-24 10:20:21 +0300

    End of section 12

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -6,15 +6,26 @@ builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-.AddJwtBearer(options =>
+    .AddJwtBearer(options =>
+    {
+        options.Authority = builder.Configuration["IdentityServiceUrl"];
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters.ValidateAudience = false;
+        options.TokenValidationParameters.NameClaimType = "username";
+    });
+builder.Services.AddCors(options =>
 {
-    options.Authority = builder.Configuration["IdentityServiceUrl"];
-    options.RequireHttpsMetadata = false;
-    options.TokenValidationParameters.ValidateAudience = false;
-    options.TokenValidationParameters.NameClaimType = "username";
+    options.AddPolicy("customPolicy", b =>
+    {
+        b.AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials().WithOrigins(builder.Configuration["ClientApp"]);
+    });
 });
 
 var app = builder.Build();
+
+app.UseCors();
 
 app.MapReverseProxy();
 
